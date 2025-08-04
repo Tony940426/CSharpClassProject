546d4a96453c52eb4b850d41cea730621e32aacc
Tony Wang <tony94042@gmail.com>
2025-08-04 20:16:16 +1000

    C Shart Practice

---
+++ CSharpClassProject/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter something!");
+
+// Declare Variable
+
+string? userInput = Console.ReadLine();
+Console.WriteLine("You entered " + userInput);
+Console.ReadKey();
